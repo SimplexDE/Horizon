6d2457a044d41f6f1ef9318d2d62c1dd8e03a79c
TearsDontFall64 <TearsDontFall.TDF@gmail.com>
2022-04-07 13:13:26 +0200

    Flight cleanup
    Flight:
      Change: Spieler -> Spieler:innen
      Change: Reihenfolge des null checks für das Target
      Change: "target" -> "tar"
      Change: Art der Definition des "operator"s

--- src/main/java/de/simplex/horizoncore/commands/utility/Flight.java
+++ src/main/java/de/simplex/horizoncore/commands/utility/Flight.java
@@ -15,55 +15,52 @@ import org.bukkit.entity.Player;
  */
 public class Flight implements CommandExecutor {
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(Main.PREFIX + "Dieser Befehl ist nur für Spieler zugänglich.");
-            return false;
-        }
+		if (sender instanceof Player operator) {
 
-        if (!(sender.hasPermission("core.flight"))) {
-            sender.sendMessage(Main.PREFIX + Main.NO_PERMISSION);
-            return false;
-        }
+			if (!sender.hasPermission("core.flight")) {
+				sender.sendMessage(Main.PREFIX + Main.NO_PERMISSION);
+				return false;
+			}
 
-        if (!(sender.hasPermission("core.flight.other"))) {
-            if (args.length == 1) {
-                sender.sendMessage(Main.PREFIX + Main.NO_PERMISSION);
-                return false;
-            }
-        }
+			if (!sender.hasPermission("core.flight.other")) {
+				if (args.length == 1) {
+					sender.sendMessage(Main.PREFIX + Main.NO_PERMISSION);
+					return false;
+				}
+			}
 
-        Player operator = (Player) sender;
-
-        if (args.length == 0) {
-            if (!(operator.getAllowFlight())) {
-                operator.setAllowFlight(true);
-                operator.sendMessage(Main.PREFIX + "Du kannst nun §aFliegen§7.");
-            } else {
-                operator.setAllowFlight(false);
-                operator.sendMessage(Main.PREFIX + "Du kannst nun nicht mehr §aFliegen§7.");
-            }
-        } else if (args.length == 1) {
-            if (Bukkit.getPlayer(args[0]) != null) {
-                Player target = Bukkit.getPlayer(args[0]);
-                if (!(target.getAllowFlight())) {
-                    target.setAllowFlight(true);
-                    target.sendMessage(Main.PREFIX + "Du kannst nun §aFliegen§7.");
-                    operator.sendMessage(Main.PREFIX + "§e" + target.getName() + " §7kann nun §aFliegen§7.");
-                } else {
-                    target.setAllowFlight(false);
-                    target.sendMessage(Main.PREFIX + "Du kannst nun nicht mehr §aFliegen§7.");
-                    operator.sendMessage(Main.PREFIX + "§e" + target.getName() + " §7kann nun nicht mehr §aFliegen§7.");
-                }
-            } else {
-                operator.sendMessage(Main.PREFIX + "Bitte gebe einen Spieler an, der Online ist.");
-            }
-        } else {
-            operator.sendMessage(Main.PREFIX + "§cNutzung: /flight [<Spieler>]");
-        }
-
-        return false;
-    }
+			if (args.length == 0) {
+				if (!operator.getAllowFlight()) {
+					operator.setAllowFlight(true);
+					operator.sendMessage(Main.PREFIX + "Du kannst nun §aFliegen§7.");
+				} else {
+					operator.setAllowFlight(false);
+					operator.sendMessage(Main.PREFIX + "Du kannst nun nicht mehr §aFliegen§7.");
+				}
+			} else if (args.length == 1) {
+				Player tar = Bukkit.getPlayer(args[0]);
+				if (tar != null) {
+					if (!tar.getAllowFlight()) {
+						tar.setAllowFlight(true);
+						tar.sendMessage(Main.PREFIX + "Du kannst nun §aFliegen§7.");
+						operator.sendMessage(Main.PREFIX + "§e" + tar.getName() + " §7kann nun §aFliegen§7.");
+					} else {
+						tar.setAllowFlight(false);
+						tar.sendMessage(Main.PREFIX + "Du kannst nun nicht mehr §aFliegen§7.");
+						operator.sendMessage(Main.PREFIX + "§e" + tar.getName() + " §7kann nun nicht mehr §aFliegen§7.");
+					}
+				} else {
+					operator.sendMessage(Main.PREFIX + "Bitte gebe eine/n Spieler:in an, der/die Online ist.");
+				}
+			} else {
+				operator.sendMessage(Main.PREFIX + "§cNutzung: /flight [<Spieler:in>]");
+			}
+		} else {
+			sender.sendMessage(Main.PREFIX + "Dieser Befehl ist nur für Spieler:innen zugänglich.");
+		}
+		return false;
+	}
 }
