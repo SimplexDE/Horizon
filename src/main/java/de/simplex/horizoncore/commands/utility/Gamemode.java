f54c3d9ed475484074c65c867d91c56ff1c6e092
Simplex <kontakt.simplex@outlook.de>
2022-04-07 13:33:59 +0200

    refactor: gamemode cleanup
    Signed-off-by: Simplex <kontakt.simplex@outlook.de>

--- src/main/java/de/simplex/horizoncore/commands/utility/Gamemode.java
+++ src/main/java/de/simplex/horizoncore/commands/utility/Gamemode.java
@@ -2,7 +2,6 @@ package de.simplex.horizoncore.commands.utility;
 
 import de.simplex.horizoncore.Main;
 import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -19,84 +18,54 @@ public class Gamemode implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        boolean gm_other = true;
+        if (sender instanceof Player p) {
 
-        // Testet, ob der Ausführende die benötigten Berechtigungen hat.
-        if (!(sender.hasPermission("core.gamemode"))) {
-            sender.sendMessage(Main.PREFIX + Main.NO_PERMISSION);
-            return false;
-        }
-
-        if (!(sender.hasPermission("core.gamemode.other"))) {
-            if (args.length == 2) {
+            if (!(sender.hasPermission("core.gamemode"))) {
                 sender.sendMessage(Main.PREFIX + Main.NO_PERMISSION);
                 return false;
             }
-        }
 
-        if (args.length == 1) {
-            Player operator = (Player) sender;
-            Player target = (Player) sender;
-            String mode = args[0];
-            setGamemode(operator, target, mode);
-        } else if (args.length == 2) {
-            Player operator = (Player) sender;
-            if (Bukkit.getPlayer(args[1]) == null) {
-                operator.sendMessage(Main.PREFIX + "§7Bitte gebe einen Spieler an, der aktuell Online ist.");
-                return false;
+            if (args.length == 1) {
+                org.bukkit.GameMode gm = org.bukkit.GameMode.valueOf(args[0]);
+                if (gm == null)
+                    gm = org.bukkit.GameMode.getByValue(Integer.parseInt(args[0]));
+                if (gm == null) {
+                    p.sendMessage(Main.PREFIX + "Dieser Spielmodus existiert nicht!");
+                    return true;
+                }
+                p.sendMessage(Main.PREFIX + String.format("Dein Spielmodus ist nun §a%s§8.", gm.toString().toLowerCase()));
+
+            } else if (args.length == 2) {
+
+                if (!p.hasPermission("core.gamemode.other")) {
+                    sender.sendMessage(Main.PREFIX + Main.NO_PERMISSION);
+                    return false;
+                }
+
+                org.bukkit.GameMode gm = org.bukkit.GameMode.valueOf(args[0]);
+                Player target = Bukkit.getPlayer(args[1]);
+                if (gm == null)
+                    gm = org.bukkit.GameMode.getByValue(Integer.parseInt(args[0]));
+                if (gm == null) {
+                    p.sendMessage(Main.PREFIX + "Dieser Spielmodus existiert nicht!");
+                    return true;
+                }
+
+                if (target == null)
+                    target = Bukkit.getPlayer(args[0]);
+                if (target == null) {
+                    p.sendMessage(Main.PREFIX + "Dieser Spieler ist nicht online!");
+                    return true;
+                }
+                target.sendMessage(Main.PREFIX + String.format("Dein Spielmodus ist nun §a%s§8.", gm.toString().toLowerCase()));
+                p.sendMessage(Main.PREFIX + String.format("Der Spielmodus von §e%s§7 ist nun §a%s§8.", target.getName(), gm.toString().toLowerCase()));
+            } else {
+                p.sendMessage(Main.PREFIX + "Diese Argumentenlänge ist ungültig.");
             }
-            Player target = Bukkit.getPlayer(args[1]);
-            String mode = args[0];
-            setGamemode(operator, target, mode);
+        } else {
+            sender.sendMessage(Main.PREFIX + "Dieser Befehl ist nur für Spieler:innen zugänglich.");
         }
-
         return false;
     }
 
-    /**
-     * Gamemode setzen
-     *
-     * @param operator  ausführender Nutzer
-     * @param target    Ziel
-     * @param givenMode Angegebener Modus
-     */
-    public void setGamemode(Player operator, Player target, String givenMode) {
-
-        String modeName = null;
-        GameMode mode = null;
-        switch (givenMode) {
-            case "0":
-                modeName = "§aÜberleben";
-                mode = GameMode.SURVIVAL;
-                break;
-            case "1":
-                modeName = "§aKreativ";
-                mode = GameMode.CREATIVE;
-                break;
-            case "2":
-                modeName = "§aAbenteuer";
-                mode = GameMode.ADVENTURE;
-                break;
-            case "3":
-                modeName = "§aZuschauer";
-                mode = GameMode.SPECTATOR;
-                break;
-            default:
-                operator.sendMessage(Main.PREFIX + "Dieser Spielmodus existiert nicht.");
-                break;
-        }
-
-        if (mode != null) {
-            if (operator == target) {
-                target.setGameMode(mode);
-                target.sendMessage(Main.PREFIX + "Dein Spielmodus ist nun " + modeName + "§7.");
-            } else {
-                target.setGameMode(mode);
-                target.sendMessage(Main.PREFIX + "Dein Spielmodus ist nun " + modeName + "§7.");
-                operator.sendMessage(Main.PREFIX + "Der Spielmodus von §e" + target.getName() + "§7 ist nun " + modeName + "§7.");
-            }
-        }
-    }
-
 }
-
