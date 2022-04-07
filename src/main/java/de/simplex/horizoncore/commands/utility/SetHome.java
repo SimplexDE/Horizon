142455d8385f4a56bef64aab56723e205cb141b4
Simplex <kontakt.simplex@outlook.de>
2022-04-07 21:48:54 +0200

    fix: Fix #3.1 for sethome.
    Sethome:
    - Ich hab kurzer hand das zu Ende Programmieren vergessen.

--- src/main/java/de/simplex/horizoncore/commands/utility/SetHome.java
+++ src/main/java/de/simplex/horizoncore/commands/utility/SetHome.java
@@ -33,8 +33,11 @@ public class SetHome implements CommandExecutor {
                             p.sendMessage(Main.PREFIX + "Du kannst nicht über mehr als 3 Homes setzen!");
                             return false;
                         }
-                    if (name == pC.getString(name))
-                        pC.set("homes", homes += 1);
+                    if (name == pC.getString(name)) {
+                        p.sendMessage(Main.PREFIX + "Dieser Name ist bereits benutzt.");
+                        return false;
+                    }
+                    pC.set("homes", homes += 1);
                     pC.set("homelist." + name + ".X", loc.getX());
                     pC.set("homelist." + name + ".Y", loc.getY());
                     pC.set("homelist." + name + ".Z", loc.getZ());
