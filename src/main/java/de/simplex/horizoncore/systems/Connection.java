89c6f49ff321ab2cd486607636ec0307011476ac
TearsDontFall64 <TearsDontFall.TDF@gmail.com>
2022-04-08 07:19:53 +0200

    Default Kit
    Connection:
     Add: Default kid

--- src/main/java/de/simplex/horizoncore/systems/Connection.java
+++ src/main/java/de/simplex/horizoncore/systems/Connection.java
@@ -3,12 +3,15 @@ package de.simplex.horizoncore.systems;
 import de.simplex.horizoncore.Main;
 import de.simplex.horizoncore.commands.api.AchievementAPI;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -44,6 +47,16 @@ public class Connection implements Listener {
 		if (!player.hasPlayedBefore()) {
 			pC.setTokens(1000);
 			player.sendMessage(Main.PREFIX + "Da du das erste Mal gejoined bist, erhälst du §e1000§7 Tokens!");
+
+			PlayerInventory i = player.getInventory();
+			i.addItem(new ItemStack(Material.COOKED_BEEF));
+			i.setChestplate(new ItemStack(Material.LEATHER_CHESTPLATE));
+			i.addItem(new ItemStack(Material.STONE_SWORD));
+			i.addItem(new ItemStack(Material.STONE_PICKAXE));
+			i.addItem(new ItemStack(Material.STONE_AXE));
+			i.addItem(new ItemStack(Material.STONE_SHOVEL));
+			i.addItem(new ItemStack(Material.STONE_HOE));
+			player.sendMessage(Main.PREFIX + "Da du das erste Mal gejoined bist, hast du das standard Kit erhalten!");
 		}
 
 		Sb.defaultSb(player);
