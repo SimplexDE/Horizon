f09f1caee39690f9fc86b042b8353e873c10b80d
Simplex <kontakt.simplex@outlook.de>
2023-03-04 00:48:52 +0100

    refactor: Fixed Color-Problem, implemented new Color-System, removed HexConverter
    Author: WillyButz

--- src/main/java/de/simplex/horizon/method/Sidebar.java
+++ src/main/java/de/simplex/horizon/method/Sidebar.java
@@ -1,5 +1,6 @@
 package de.simplex.horizon.method;
 
+import de.simplex.horizon.enums.Color;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
@@ -22,8 +23,7 @@ public class Sidebar {
             .build();
 
     public static void defaultSb(Player p) {
-
-        Component displayComponent = MiniMessage.miniMessage().deserialize("<#43adf2>   ʜᴏʀɪᴢᴇɴ ᴍʏsᴇʀᴠᴇʀ   ");
+        Component displayComponent = MiniMessage.miniMessage().deserialize("   <rainbow>ʜᴏʀɪᴢᴏɴ ᴍʏsᴇʀᴠᴇʀ</rainbow>   ");
         String displayString = legacySerializer.serialize(displayComponent);
 
         Scoreboard sb = Bukkit.getScoreboardManager().getNewScoreboard();
@@ -38,9 +38,20 @@ public class Sidebar {
                 s3 = o.getScore(" §2 "),
                 s4 = o.getScore(" §3 ");
 
-        Score Welt = o.getScore("§8» §7Welt"),
-                Rang = o.getScore("§8» §7Rang"),
-                Spieler = o.getScore("§8» §7Spieler");
+        Component rangtitleComponent = MiniMessage.miniMessage()
+                .deserialize(Color.YELLOW.getColor() + "» " + Color.LIGHT_GRAY.getColor() + "Dein Rang");
+        Component worldtitleComponent = MiniMessage.miniMessage()
+                .deserialize(Color.YELLOW.getColor() + "» " + Color.LIGHT_GRAY.getColor() + "Aktuelle Welt");
+        Component playertitleComponent = MiniMessage.miniMessage()
+                .deserialize(Color.YELLOW.getColor() + "» " + Color.LIGHT_GRAY.getColor() + "Aktuelle Spieler");
+
+        String rangtitleRow = legacySerializer.serialize(rangtitleComponent);
+        String worldtitleRow = legacySerializer.serialize(worldtitleComponent);
+        String playertitleRow = legacySerializer.serialize(playertitleComponent);
+
+        Team Welt = sb.registerNewTeam(worldtitleRow),
+                Rang = sb.registerNewTeam(rangtitleRow),
+                Spieler = sb.registerNewTeam(playertitleRow);
 
         Team TWelt = sb.registerNewTeam("TWelt");
         Team TRang = sb.registerNewTeam("TRang");
@@ -49,6 +60,9 @@ public class Sidebar {
         TWelt.addEntry("§0");
         TRang.addEntry("§1");
         TSpieler.addEntry("§2");
+        Welt.addEntry("§3");
+        Rang.addEntry("§4");
+        Spieler.addEntry("§5");
 
         User u = lpapi.getUserManager().getUser(p.getUniqueId());
         Group g = lpapi.getGroupManager().getGroup(u.getPrimaryGroup());
@@ -57,10 +71,11 @@ public class Sidebar {
 
         Component rangComponent = MiniMessage.miniMessage().deserialize(color + g.getDisplayName());
         Component worldComponent = MiniMessage.miniMessage()
-                .deserialize("<#4ffe41>" + StringUtils.capitalize(world.getName()));
+                .deserialize(Color.LIGHT_GREEN.getColor() + StringUtils.capitalize(p.getWorld().getName()));
         Component playerComponent = MiniMessage.miniMessage()
-                .deserialize("<#4ffe41>" + Bukkit.getOnlinePlayers().size()
-                        + "<#AAAAAA> / <#3abd30>" + Bukkit.getMaxPlayers());
+                .deserialize(Color.LIGHT_GREEN.getColor() +
+                        Bukkit.getOnlinePlayers().size() + Color.DARK_GRAY.getColor() + " / " + Color.GREEN.getColor()
+                        + Bukkit.getMaxPlayers());
 
         String rangRow = legacySerializer.serialize(rangComponent);
         String worldRow = legacySerializer.serialize(worldComponent);
@@ -69,16 +84,19 @@ public class Sidebar {
         TWelt.setPrefix(worldRow);
         TRang.setPrefix(rangRow);
         TSpieler.setPrefix(playerRow);
+        Welt.setPrefix(worldtitleRow);
+        Rang.setPrefix(rangtitleRow);
+        Spieler.setPrefix(playertitleRow);
 
         //Title.setScore(10);
         s4.setScore(9);
-        Rang.setScore(8);
+        o.getScore("§4").setScore(8);
         o.getScore("§1").setScore(7);
         s3.setScore(6);
-        Spieler.setScore(5);
+        o.getScore("§5").setScore(5);
         o.getScore("§2").setScore(4);
         s2.setScore(3);
-        Welt.setScore(2);
+        o.getScore("§3").setScore(2);
         o.getScore("§0").setScore(1);
         s1.setScore(0);
 
@@ -89,15 +107,21 @@ public class Sidebar {
         World world = p.getWorld();
         User u = lpapi.getUserManager().getUser(p.getUniqueId());
         Group g = lpapi.getGroupManager().getGroup(u.getPrimaryGroup());
+        String DisplayName = g.getDisplayName();
+
+        if (DisplayName == "null") {
+            DisplayName = g.getName();
+        }
 
         String color = StringUtils.substring(u.getCachedData().getMetaData().getPrefix(), 1, 8);
 
-        Component rangComponent = MiniMessage.miniMessage().deserialize(color + g.getDisplayName());
+        Component rangComponent = MiniMessage.miniMessage().deserialize(color + DisplayName);
         Component worldComponent = MiniMessage.miniMessage()
-                .deserialize("<#4ffe41>" + StringUtils.capitalize(world.getName()));
+                .deserialize(Color.LIGHT_GREEN.getColor() + StringUtils.capitalize(p.getWorld().getName()));
         Component playerComponent = MiniMessage.miniMessage()
-                .deserialize("<#4ffe41>" + Bukkit.getOnlinePlayers().size()
-                        + "<#AAAAAA> / <#3abd30>" + Bukkit.getMaxPlayers());
+                .deserialize(Color.LIGHT_GREEN.getColor() +
+                        Bukkit.getOnlinePlayers().size() + Color.DARK_GRAY.getColor() + " / " + Color.GREEN.getColor()
+                        + Bukkit.getMaxPlayers());
         String rangRow = legacySerializer.serialize(rangComponent);
         String worldRow = legacySerializer.serialize(worldComponent);
         String playerRow = legacySerializer.serialize(playerComponent);
@@ -109,18 +133,31 @@ public class Sidebar {
     }
 
     public static void updateWorld(@NotNull Player p) {
+
+        Component worldComponent = MiniMessage.miniMessage().deserialize(Color.LIGHT_GREEN.getColor() + StringUtils.capitalize(p.getWorld().getName()));
+        String worldRow = legacySerializer.serialize(worldComponent);
+
         Scoreboard sb = p.getScoreboard();
         sb.getTeam("TWelt")
-                .setPrefix(HexConverter.getColString("<#4ffe41>" + StringUtils.capitalize(p.getWorld().getName())));
+                .setPrefix(worldRow);
     }
 
     public static void updateRank(@NotNull Player p) {
         User u = lpapi.getUserManager().getUser(p.getUniqueId());
         Group g = lpapi.getGroupManager().getGroup(u.getPrimaryGroup());
+        String DisplayName = g.getDisplayName();
+
+        if (DisplayName == null) {
+            DisplayName = g.getName();
+        }
+
 
         String color = StringUtils.substring(u.getCachedData().getMetaData().getPrefix(), 0, 9);
+        if (color == null) {
+            color = "<#949494>";
+        }
 
-        Component rangComponent = MiniMessage.miniMessage().deserialize(color + g.getDisplayName());
+        Component rangComponent = MiniMessage.miniMessage().deserialize(color + DisplayName);
         String rangRow = legacySerializer.serialize(rangComponent);
 
         Scoreboard sb = p.getScoreboard();
@@ -131,16 +168,24 @@ public class Sidebar {
     }
 
     public static void updatePlayers(@NotNull Player p) {
+        Component playerComponent = MiniMessage.miniMessage().deserialize(Color.LIGHT_GREEN.getColor() +
+                Bukkit.getOnlinePlayers().size() + Color.DARK_GRAY.getColor() + " / " + Color.GREEN.getColor()
+                + Bukkit.getMaxPlayers());
+        String playerRow = legacySerializer.serialize(playerComponent);
+
         Scoreboard sb = p.getScoreboard();
         sb.getTeam("TSpieler")
-                .setPrefix(HexConverter.getColString("<#4ffe41>" + Bukkit.getOnlinePlayers().size()
-                        + "<#AAAAAA> / <#3abd30>" + Bukkit.getMaxPlayers()));
+                .setPrefix(playerRow);
     }
 
     public static void unsetScoreboard(@NotNull Player p) {
+
+        Component titleComponent = MiniMessage.miniMessage().deserialize(Color.LIGHT_BLUE.getColor() + "ʜᴏʀɪᴢᴏɴ ᴍʏsᴇʀᴠᴇʀ");
+        String titleRow = legacySerializer.serialize(titleComponent);
+
         Scoreboard sb = Bukkit.getScoreboardManager().getNewScoreboard();
         Objective o = sb.registerNewObjective("hor", "hor",
-                HexConverter.getColString("<#43adf2>ʜᴏʀɪᴢᴇɴ ᴍʏsᴇʀᴠᴇʀ"));
+                titleRow);
         p.setScoreboard(sb);
     }
 }
