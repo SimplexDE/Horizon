7db0e1447f70cba84ee24fba851b2ffbe9849413
Simplex <kontakt.simplex@outlook.de>
2023-03-04 21:36:09 +0100

    feat: Tablist sorting finally works!
    Author: WillyButz

--- src/main/java/de/simplex/horizon/method/Tablist.java
+++ src/main/java/de/simplex/horizon/method/Tablist.java
@@ -6,10 +6,15 @@ import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.luckperms.api.model.group.Group;
 import net.luckperms.api.model.user.User;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 import org.jetbrains.annotations.NotNull;
+
+import static de.simplex.horizon.commands.api.LuckPermsAPI.lpapi;
 
 public class Tablist {
 
@@ -47,6 +52,28 @@ public class Tablist {
     public void setNameTag(@NotNull Player player, @NotNull User u) {
         String prefix = u.getCachedData().getMetaData().getPrefix();
         String suffix = u.getCachedData().getMetaData().getSuffix();
+        Group g = lpapi.getGroupManager().getGroup(u.getPrimaryGroup());
+        Scoreboard sb = player.getScoreboard();
+
+
+        for (Player ap : Bukkit.getOnlinePlayers()) {
+            User au = lpapi.getUserManager().getUser(ap.getUniqueId());
+            Group gap = lpapi.getGroupManager().getGroup(au.getPrimaryGroup());
+            int priority = gap.getWeight().getAsInt();
+            String groupName = gap.getFriendlyName();
+            String playerName = ap.getName();
+            if (groupName.length() > 15) {
+                groupName = groupName.substring(0, 15);
+            }
+            char priorityChar = (char) (19968 + priority);
+            String teamName = priorityChar + groupName;
+            Team team = sb.getTeam(teamName);
+            if (team == null) {
+                team = sb.registerNewTeam(teamName);
+            }
+            team.addEntry(playerName);
+        }
+
 
         Component prefixComponent = MiniMessage.miniMessage().deserialize(prefix + player.getName() + suffix);
         String prefixString = legacySerializer.serialize(prefixComponent);
