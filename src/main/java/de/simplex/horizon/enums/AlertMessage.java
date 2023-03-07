77fc2857222b2e558b524c4fc312ec74954d7ff5
Simplex <kontakt.simplex@outlook.de>
2023-03-07 22:54:09 +0100

    feat: Added MISSING_ARGUMENT Message
    Author: WillyButz

--- src/main/java/de/simplex/horizon/enums/AlertMessage.java
+++ src/main/java/de/simplex/horizon/enums/AlertMessage.java
@@ -2,6 +2,7 @@ package de.simplex.horizon.enums;
 
 public enum AlertMessage {
     INVALID_ARGUMENT_LENGTH(ResponseMessage.ERROR.getNotification() + "Invalid argument length!"),
+    MISSING_ARGUMENT(ResponseMessage.WARN.getNotification() + "Missing argument!"),
     INVALID_ARGUMENT(ResponseMessage.WARN.getNotification() + "Invalid argument passed!"),
     ONLY_PLAYER(ResponseMessage.ERROR.getNotification() + "Only runnable as player!"),
     PLAYER_NOT_FOUND(ResponseMessage.WARN.getNotification() + "This player doesn't exist or isn't online!"),
@@ -13,7 +14,7 @@ public enum AlertMessage {
         this.msg = msg;
     }
 
-    public String getMsg() {
+    public String getMessage() {
         return msg;
     }
 }
