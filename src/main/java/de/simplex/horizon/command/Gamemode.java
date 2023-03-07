82097673ee154da7adeaeff4e6d25154591a2644
Simplex <kontakt.simplex@outlook.de>
2023-03-07 23:24:19 +0100

    Revert "feat: Added Gamemode & Enderchest Command"
    This reverts commit d488bedcb13d39f15cf8578a3f4ea1d7571e16d0.

--- src/main/java/de/simplex/horizon/command/Gamemode.java
+++ src/main/java/de/simplex/horizon/command/Gamemode.java
@@ -1,10 +1,11 @@
-package de.simplex.horizon.commands;
+package de.simplex.horizon.command;
 
+import de.simplex.horizon.enums.AlertMessage;
 import de.simplex.horizon.enums.Color;
 import de.simplex.horizon.enums.GameModeName;
-import de.simplex.horizon.enums.Messages;
-import de.simplex.horizon.enums.Notification;
+import de.simplex.horizon.enums.ResponseMessage;
 import de.simplex.horizon.util.MessageSender;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
@@ -21,57 +22,67 @@ public class Gamemode implements TabExecutor {
 
     MessageSender ms = new MessageSender();
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (args.length == 1) {
-            if (sender instanceof Player p) {
-                GameModeName modeName = GameModeName.fromString(args[0]);
-
-                if (modeName != null) {
-                    GameMode gameMode = modeName.getGameMode();
-                    p.setGameMode(gameMode);
-                    ms.sendToPlayer(p, Notification.SYSTEM.getNotification() + "Gamemode set to "
-                            + Color.GREEN.getColorMiniMessage() + modeName.toString().toUpperCase());
-                } else {
-                    ms.sendToSender(sender, Messages.INVALID_ARGUMENT.getMsg());
-                    return false;
-                }
-                return true;
-            } else {
-                ms.sendToSender(sender, Messages.ONLY_PLAYER.getMsg());
-                return false;
-            }
-        } else if (args.length == 2) {
-            if (Bukkit.getPlayer(args[1]) != null) {
-                Player target = Bukkit.getPlayer(args[1]);
-                GameModeName modeName = GameModeName.fromString(args[0]);
-
-                if (modeName != null) {
-                    GameMode gameMode = modeName.getGameMode();
-                    target.setGameMode(gameMode);
-                    ms.sendToPlayer(target, Notification.SYSTEM.getNotification() + "Gamemode set to "
-                            + Color.GREEN.getColorMiniMessage() + modeName.toString().toUpperCase());
-                    ms.sendToSender(sender, Notification.SYSTEM.getNotification() + "Gamemode of " + target.getName() + " was set to "
-                            + Color.GREEN.getColorMiniMessage() + modeName.toString().toUpperCase());
-                } else {
-                    ms.sendToSender(sender, Messages.INVALID_ARGUMENT.getMsg());
-                    return false;
-                }
-                return true;
-            } else {
-                ms.sendToSender(sender, Messages.PLAYER_NOT_ONLINE.getMsg());
-                return true;
-            }
-        } else {
-            ms.sendToSender(sender, Messages.INVALID_ARGUMENT_LENGTH.getMsg());
-            return false;
-        }
+    private void setGameMode(Player player, GameModeName gameMode) {
+        player.setGameMode(gameMode.getGameMode());
+        ms.sendToPlayer(player, ResponseMessage.INFO.getNotification() + "Gamemode changed to "
+              + Color.LIGHT_GREEN.getColorMiniMessage() + StringUtils.capitalize(gameMode.getGameModeName()));
     }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        final String commandPermission = command.getPermission();
+
+        Player targetPlayer;
+
+        GameModeName gameMode;
+
+        if (args.length > 2) {
+            ms.sendToSender(sender, AlertMessage.INVALID_ARGUMENT_LENGTH.getMessage());
+            return true;
+        }
+
+        if (GameModeName.fromString(args[0]) == null) {
+            ms.sendToSender(sender, AlertMessage.INVALID_ARGUMENT.getMessage());
+            return true;
+        }
+        gameMode = GameModeName.fromString(args[0]);
+
+        if (args.length == 2 && Bukkit.getPlayer(args[1]) != null && Bukkit.getPlayer(args[1]) != sender) {
+            if (!sender.hasPermission(commandPermission + ".others")) {
+                ms.sendToSender(sender, AlertMessage.NO_PERMISSION.getMessage());
+                return true;
+            }
+            targetPlayer = Bukkit.getPlayer(args[1]);
+
+            setGameMode(targetPlayer, gameMode);
+            ms.sendToSender(sender, ResponseMessage.INFO.getNotification() + "Gamemode of "
+                  + targetPlayer.getName() + " changed to " + Color.LIGHT_GREEN.getColorMiniMessage()
+                  + StringUtils.capitalize(StringUtils.capitalize(gameMode.getGameModeName())));
+            return true;
+        }
+
+        if (!(sender instanceof Player player)) {
+            ms.sendToSender(sender, AlertMessage.ONLY_PLAYER.getMessage());
+            return true;
+        }
+
+        setGameMode(player, gameMode);
+        return true;
+    }
+
 
     @Nullable
     @Override
-    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (args.length == 2) {
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
+                                      @NotNull String[] args) {
+        if (args.length == 1) {
+            List<String> gameModes = new ArrayList<>();
+            for (GameMode gamemode : GameMode.values()) {
+                gameModes.add(gamemode.toString().toLowerCase());
+            }
+            return gameModes;
+        } else if (args.length == 2) {
             List<String> playerNames = new ArrayList<>();
             Player[] players = new Player[Bukkit.getServer().getOnlinePlayers().size()];
             Bukkit.getServer().getOnlinePlayers().toArray(players);
@@ -79,13 +90,8 @@ public class Gamemode implements TabExecutor {
                 playerNames.add(players[i].getName());
             }
             return playerNames;
+        } else {
+            return null;
         }
-        if (args.length == 1) {
-            List<String> gameModes = new ArrayList<>();
-            for (GameMode gamemode : GameMode.values()) {
-                gameModes.add(gamemode.toString());
-            }
-        }
-        return null;
     }
 }
