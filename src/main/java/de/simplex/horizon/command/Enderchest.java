e72875c4d6e5ec8d5d7c7cec79dd64ba46db0c1a
Simplex <kontakt.simplex@outlook.de>
2023-03-07 22:56:56 +0100

    refactor: Rewritten Enderchest.class
    Author: WillyButz

--- src/main/java/de/simplex/horizon/command/Enderchest.java
+++ src/main/java/de/simplex/horizon/command/Enderchest.java
@@ -1,12 +1,14 @@
 package de.simplex.horizon.command;
 
 import de.simplex.horizon.enums.AlertMessage;
+import de.simplex.horizon.enums.ResponseMessage;
 import de.simplex.horizon.util.MessageSender;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -19,37 +21,32 @@ public class Enderchest implements TabExecutor {
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
 	                         @NotNull String[] args) {
-		if (args.length == 0) {
-			if (sender instanceof Player p) {
-				p.openInventory(p.getEnderChest());
-				return true;
-			} else {
-				ms.sendToSender(sender, AlertMessage.ONLY_PLAYER.getMsg());
-				return true;
-			}
-		} else if (args.length == 1) {
-			if (Bukkit.getPlayer(args[0]) != null) {
-				if (sender instanceof Player p) {
-					if (sender.hasPermission("server.enderchest.others")) {
-						Player target = Bukkit.getPlayer(args[0]);
-						p.openInventory(target.getEnderChest());
-						return true;
-					} else {
-						ms.sendToSender(sender, AlertMessage.NO_PERMISSION.getMsg());
-						return true;
-					}
-				} else {
-					ms.sendToSender(sender, AlertMessage.ONLY_PLAYER.getMsg());
-					return true;
-				}
-			} else {
-				ms.sendToSender(sender, AlertMessage.PLAYER_NOT_FOUND.getMsg());
-				return true;
-			}
-		} else {
-			ms.sendToSender(sender, AlertMessage.INVALID_ARGUMENT_LENGTH.getMsg());
-			return false;
+		Player targetPlayer;
+		Inventory targetEnderChest;
+
+		if (args.length > 1) {
+			ms.sendToSender(sender, AlertMessage.INVALID_ARGUMENT_LENGTH.getMessage());
+			return true;
 		}
+
+		if (!(sender instanceof Player player)) {
+			ms.sendToSender(sender, AlertMessage.ONLY_PLAYER.getMessage());
+			return true;
+		}
+
+		if (args.length == 1 && Bukkit.getPlayer(args[0]) != null && Bukkit.getPlayer(args[0]) != sender) {
+			targetPlayer = Bukkit.getPlayer(args[0]);
+			targetEnderChest = targetPlayer.getEnderChest();
+			player.openInventory(targetEnderChest);
+			ms.sendToPlayer(player,
+				ResponseMessage.INFO.getNotification() + "Enderchest of " + targetPlayer.getName() + "opened");
+			return true;
+		}
+
+		targetEnderChest = player.getEnderChest();
+		player.openInventory(targetEnderChest);
+		ms.sendToPlayer(player, ResponseMessage.INFO.getNotification() + "Enderchest opened");
+		return true;
 	}
 
 	@Nullable
