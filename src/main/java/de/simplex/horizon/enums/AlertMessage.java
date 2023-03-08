d4e89d594d0c09ab286a16337bb8976f2e33e3c6
Simplex <kontakt.simplex@outlook.de>
2023-03-08 19:09:52 +0100

    refactor: Changed some Warning levels
    Author: WillyButz

--- src/main/java/de/simplex/horizon/enums/AlertMessage.java
+++ src/main/java/de/simplex/horizon/enums/AlertMessage.java
@@ -2,11 +2,11 @@ package de.simplex.horizon.enums;
 
 public enum AlertMessage {
     INVALID_ARGUMENT_LENGTH(ResponseMessage.ERROR.getNotification() + "Invalid argument length!"),
-    MISSING_ARGUMENT(ResponseMessage.WARN.getNotification() + "Missing argument!"),
-    INVALID_ARGUMENT(ResponseMessage.WARN.getNotification() + "Invalid argument passed!"),
+    MISSING_ARGUMENT(ResponseMessage.ERROR.getNotification() + "Missing argument!"),
+    INVALID_ARGUMENT(ResponseMessage.ERROR.getNotification() + "Invalid argument passed!"),
     ONLY_PLAYER(ResponseMessage.ERROR.getNotification() + "Only runnable as player!"),
     PLAYER_NOT_FOUND(ResponseMessage.WARN.getNotification() + "This player doesn't exist or isn't online!"),
-    NO_PERMISSION(ResponseMessage.INFO.getNotification() + "You are lacking the required permission!");
+    NO_PERMISSION(ResponseMessage.ERROR.getNotification() + "You are lacking the required permission!");
 
     private String msg;
 
